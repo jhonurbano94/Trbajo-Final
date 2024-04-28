f9e19dd6c8acfa31734e64f6ad25c9aa13a68ea1
unknown <jhonurbano094@gmail.com>
2024-04-27 20:01:33 -0500

    27-04-2024

---
+++ public/styles/registro.js
@@ -0,0 +1,14 @@
+// 1 . verificar conexion con html 
+console. log("holaaaa soy registro");
+
+
+//2. crear mi funcion para registro 
+
+const registarUsuario = async () => {
+    //2.1 obtener los datos de mi formulario
+    const Nombres= document.getElementById("Nombres").value;
+    const Apellidos = document.getElementById("Apellidos").value;
+    const Correo = document.getElementById("Correo").value;
+    const Correo2 = Document.getElementById("Correo2").value;
+
+}
